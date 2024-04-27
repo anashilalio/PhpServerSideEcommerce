3cfbd833875647484b0ec834f48a3bbeaad3afd8
anashilalio <anaslike39@gmail.com>
2024-04-27 20:34:02 +0100

    change

--- client/login.php
+++ client/login.php
@@ -1,4 +1,23 @@
 <?php
     header("Access-Control-Allow-Origin: http://localhost:5173");
     header("Access-Control-Allow-Headers: *");
+
+    $postData = file_get_contents("php://input");
+    $request = json_decode($postData);
+    $username= $request->username;
+    $password = $request->password;
+    $db = new PDO('mysql:host=localhost;dbname=e-commerce','root' ,'');
+    $stmt = $db->prepare("SELECT * FROM client WHERE username = ? AND pswd = ? ");
+    $stmt->execute([$username , $password]);
+    if($stmt->rowCount()>0){
+        $response = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }else{
+        echo "user doesn't exist";
+        header('Content-Type: application/json');
+    }
+    
+
 ?>
